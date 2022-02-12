7674e8d36be39fbe7f5e176795f1f54ddaa52ee3
kenstratton <ken.stratton.2643@mail.linbenton.edu>
2022-02-12 17:54:22 +0900

    add tests

--- list.cpp
+++ list.cpp
@@ -8,9 +8,8 @@ int len = 0;
 
 // Structure of a list element
 struct ELMT {
-    // int id;            // Element Data 1: Location (positive integer)
-    int value;         // Element Data 2: Integer value
-    struct ELMT *next; // Element Data 3: Pointer indicates an address of a linked element OR NULL
+    int value;         // Element Data 1: Integer value
+    struct ELMT *next; // Element Data 2: Pointer indicates an address of a linked element OR NULL
 };
 
 static struct ELMT *head = NULL; // A head element in a list
@@ -28,7 +27,7 @@ bool isEmpty() {
 
 /*******************************************
  Search fuction :
- * 1. Search a element with a given value (check one -> go to a next element)
+ * 1. Search a element based on a given value (check one -> go to a next element)
  * 2. Return a element position OR 0(not found)
 ********************************************/
 int search(int targetVal) {
@@ -47,13 +46,13 @@ int search(int targetVal) {
 
 /*******************************************
  Search fuction 2 :
- * 1. Search a element with a given position
+ * 1. Search a element based on a given position
  * 2. Return a element value
 ********************************************/
 int search2(int targetPos) {
     // * 1 
     elmt = head;
-    for (size_t i = 0; i < targetPos; i++) elmt = elmt->next;
+    for (size_t i = 1; i < targetPos; i++) elmt = elmt->next;
     return elmt->value; // * 2
 }
 
@@ -133,30 +132,33 @@ bool remove(int targetVal) {
     return false; // * 3
 }
 
-// Execute search and print a result
-void exSearch(int targetVal) {
+
+// Execute value-based search and print a result
+bool exSearch(int targetVal) {
     if (isEmpty()) {
         printf("(S) List is Empty\n");
     }
     else {
         int pos = search(targetVal);
-
         if (pos == 0) {
             printf("(S) Value %d : Not Found\n", targetVal);
         }
-        else if (pos > 0) {
+        else {
             string end;
             if (pos == 1) end = "st";
             if (pos == 2) end = "nd";
             if (pos == 3) end = "rd";
             if (pos > 3) end = "th";
-            // printf("(S) Value %d : Found! at %d%s", targetVal, pos, end);
-            cout << "(S) Value " << targetVal << " : Found! at " << pos << end << endl;
+            printf("(S) Value %d : Found! at %d%s\n", targetVal, pos, end.c_str());
+            return true;
         }
     }
+    return false;
 }
 
-void exSearch2(int targetPos) {
+
+// Execute position-based search and print a result
+bool exSearch2(int targetPos) {
     if (isEmpty()) {
         printf("(S) List is Empty\n");
     }
@@ -166,72 +168,158 @@ void exSearch2(int targetPos) {
     else {
         int value = search2(targetPos);
         printf("(S) Position %d : %d\n", targetPos, value);
+        return true;
     }
+    return false;
 }
 
 
 // Execute addtion and print a result
-void exAdd(int value) {
+bool exAdd(int value) {
     string result = add(value);
-    if (result == "ex") printf("(A) Value %d : Already Exists\n", value);
-    if (result == "f") printf("(A) Value %d : Addition Failure\n", value);
-    if (result == "s") printf("(A) Value %d : Successfully Added!\n", value);
+    if (result == "ex") {
+        printf("(A) Value %d : Already Exists\n", value);
+    }
+    else if (result == "f") {
+        printf("(A) Value %d : Addition Failure\n", value);
+    }
+    else if (result == "s") {
+        printf("(A) Value %d : Successfully Added!\n", value);
+        return true;
+    }
+    return false;
 }
 
 
 // Execute removal and print a result
-void exRemove(int targetVal) {
+bool exRemove(int targetVal) {
     if (isEmpty()) {
          printf("(R) List is Empty\n");
     }
     else {
         bool result = remove(targetVal);
-        if (result == true) printf("(R) Value %d : Removed!\n", targetVal);
-        if (result == false) printf("(R) Value %d : Not Found\n", targetVal);
+        if (result == false) {
+            printf("(R) Value %d : Not Found\n", targetVal);
+        }
+        else {
+            printf("(R) Value %d : Removed!\n", targetVal);
+            return true;
+        }
     }
+    return false;
 }
 
 
 // Check a length of a list
-void checkLength() {
+int checkLength() {
     printf(" -  List Length : %d\n", len);
+    return len;
 }
 
 
-// User Operation
-int main() {
+/*******************************************
+Test fuction :
+ * 1. Prepare variables for tests
+ * 2. Test add, remove, search, search2, isEmpty, and checkLength functions
+ * 3. Test executing functions of * 2 items
+********************************************/
+void testList() {
+    // * 1
     int val1 = 10;
     int val2 = 20;
     int val3 = 30;
     int val4 = 40;
     int val5 = 50;
+    int pos1 = 1;
+    int pos2 = 2;
+    int pos3 = 3;
+    int pos4 = 4;
 
-    // Add
-    exAdd(val5);
-    exAdd(val1);
-    exAdd(val3);
-    exAdd(val4);
-    exAdd(val2);
-    exSearch(val1);
-    exSearch2(val2);
-    exSearch(val3);
-    exSearch2(val4);
-    exSearch(val5);
-    exSearch(51);
-    checkLength();
+    printf("Tests start!\n\n");
 
-    // Remove
-    exRemove(11);
-    exRemove(val1);
-    exRemove(val3);
-    exRemove(val2);
-    exRemove(val5);
-    exRemove(val4);
-    exRemove(44);
-    exSearch(val1);
-    exSearch(val2);
-    exSearch(val3);
-    exSearch(val4);
-    exSearch(val5);
-    checkLength();
+
+    // * 2
+    assert(len == 0);
+    assert(isEmpty() == true);
+    assert(add(val1) == "s");
+    assert(add(val1) == "ex");
+    assert(len = 1);
+    assert(isEmpty() == false);
+    assert(search(val1) == 1);
+    assert(search(val2) == 0);
+    assert(search2(pos1) == val1);
+    assert(remove(val5) == false);
+    assert(remove(val1) == true);
+    assert(len == 0);
+
+
+    // * 3
+    // Execute add
+    assert(checkLength() == 0);
+    assert(exAdd(val3) == true);
+    assert(exAdd(val1) == true);
+    assert(exAdd(val4) == true);
+    assert(exAdd(val2) == true);
+    assert(exAdd(val2) == false);
+    cout << "" << endl;
+
+    // Execute search (elements in a list)
+    assert(checkLength() == 4);
+    assert(exSearch(val1) == true);
+    assert(exSearch(val2) == true);
+    assert(exSearch(val3) == true);
+    assert(exSearch(val4) == true);
+    assert(exSearch2(pos1) == true);
+    assert(exSearch2(pos2) == true);
+    assert(exSearch2(pos3) == true);
+    assert(exSearch2(pos4) == true);
+
+    // Execute searching a value/position of a non-existent element
+    assert(exSearch(11) == false);
+    assert(exSearch2(5) == false);
+    cout << "" << endl;
+
+    // Execute removing a value of a non-existent element
+    assert(exRemove(val5) == false);
+    cout << "" << endl;
+
+    // Execute removeing and searching the removed element
+    assert(exRemove(val2) == true);
+    assert(exSearch(val2) == false);
+    assert(exSearch2(pos4) == false);
+    assert(checkLength() == 3);
+    cout << "" << endl;
+
+    // Execute removeing and searching the removed element
+    assert(exRemove(val3) == true);
+    assert(exSearch(val3) == false);
+    assert(exSearch2(pos3) == false);
+    assert(checkLength() == 2);
+    cout << "" << endl;
+
+    // Execute removeing and searching the removed element
+    assert(exRemove(val1) == true);
+    assert(exSearch(val1) == false);
+    assert(exSearch2(pos2) == false);
+    assert(checkLength() == 1);
+    cout << "" << endl;
+
+    // Execute removeing and searching the removed element
+    assert(exRemove(val4) == true);
+    assert(exSearch(val4) == false);
+    assert(exSearch2(pos1) == false);
+    assert(checkLength() == 0);
+    cout << "" << endl;
+
+    // Execute removeing a value of a non-existent element
+    assert(exRemove(val5) == false);
+    cout << "" << endl;
+
+    printf("Tests Done!\n");
+}
+
+
+// User Operation
+int main() {
+    testList();
 }
